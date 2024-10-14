c3fddda9a9b58c60eae2169cf4f02ea3ba98c785
Hojimurod <hojimurod675@gmail.com>
2024-10-14 15:32:24 +0500

    create html

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const title = document.querySelector('.title');
+
+const text = title.innerHTML
+title.innerHTML = ''
+
+function rec(i = 0) {
+    title.innerHTML += text[i]
+    i++
+    if (i < text.length) {
+        setTimeout(() => {
+            rec(i)
+        }, 100);
+    }
+}
+rec()
+
+const boat = document.querySelector('.boat'),
+    clouds = document.querySelectorAll('.cloud');
+
+window.addEventListener('scroll', () => {
+    boat.style.transform = `translateX(${scrollY}px)`;
+    clouds.forEach(cloud => {
+        const speed = cloud.getAttribute('speed')
+        cloud.style.transform = `translateX(${scrollY * speed}px)`;
+    })
+})
+
+const img = document.querySelector('.img'),
+    div = document.querySelector('.div');
+
+div.addEventListener('mousemove', (e) => {
+    img.style.transform = `translate(${-e.pageX / 10}px,${-e.pageY / 8}px)`
+})
+
+
+const timers = document.querySelectorAll('.timer'),
+    item = document.querySelector('.item');
+
+window.addEventListener('scroll', function scrollFunc() {
+    if (this.scrollY + 700 > item.offsetTop) {
+        timers.forEach(timer => {
+            function rec() {
+                let count = +timer.getAttribute('count')
+                timer.innerHTML++
+                if (timer.innerHTML < count) {
+                    setTimeout(() => {
+                        rec()
+                    }, 10)
+                }
+            }
+            rec()
+        })
+        window.removeEventListener("scroll", scrollFunc)
+    }
+})
+
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(btn => {
+    window.addEventListener('mousemove', (e) => {
+        let y = e.pageY - btn.offsetTop
+        let x = e.pageX - btn.offsetTop
+        btn.style.setProperty('--x', `${x}px`)
+        btn.style.setProperty('--y', `${y}px`)
+    })
+})
